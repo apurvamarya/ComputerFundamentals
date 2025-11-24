35e685c525a55dd4690c05a5edb3a0ccf2e837c8
Apurvam Arya <apurvamarya@gmail.com>
2025-11-24 17:41:29 +0530

    Delete ch1 directory

--- ch1/c12.py
+++
@@ -1,6 +0,0 @@
-a = int(input("Enter the number of which the table is required: "))
-b = int(input("Enter the amount of multiples required: "))
-i = 1
-while i<=b:
-    print("5 x ",i," = ",a*i)
-    i+=1
